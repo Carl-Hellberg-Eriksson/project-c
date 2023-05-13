ab67f1a1cc08cbf463273f4799249ec660e69895
Carl Hellberg Eriksson <c.eriksson94@gmail.com>
2023-05-13 14:03:43 +0200

    refactored links and added calculator #5

---
+++ src/app/components/Calculator.tsx
@@ -0,0 +1,112 @@
+"use client";
+import React from "react";
+import Tooltip from "@mui/material/Tooltip";
+import TextField from "@mui/material/TextField";
+
+const Calculator = () => {
+  const [hourlyWage, setHourlyWage] = React.useState(0);
+  const [hours, setHours] = React.useState(0);
+
+  const MIN_TAXABLE_MUNICIPALITY = 27000;
+  const MUNICIPALITY_TAX = 0.2;
+  const STATE_TAX = 0.05;
+  const ChangeHourlyWage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    var x = Number(event.target.value);
+    setHourlyWage(x);
+  };
+  const ChangeHours = (event: React.ChangeEvent<HTMLInputElement>) => {
+    var x = Number(event.target.value);
+    setHours(x);
+  };
+
+  const totalIncome = hourlyWage * hours;
+  const taxableMunicipality = totalIncome;
+  const municipalityTax = taxableMunicipality * MUNICIPALITY_TAX;
+  const taxableState = Math.max(0, totalIncome - MIN_TAXABLE_MUNICIPALITY);
+  const stateTax = taxableState * STATE_TAX;
+  const incomeAfterTax = totalIncome - municipalityTax - stateTax;
+  return (
+    <div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
+        <div>
+          <div className="p-4">
+            <Tooltip
+              title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+              placement="left-start"
+            >
+              <TextField
+                label="Timlön:"
+                type="text"
+                defaultValue={0}
+                onChange={ChangeHourlyWage}
+              />
+            </Tooltip>
+          </div>
+          <div className="p-4">
+            <Tooltip
+              title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+              placement="left-start"
+            >
+              <TextField
+                label="Antal timmar:"
+                type="text"
+                defaultValue={0}
+                onChange={ChangeHours}
+              />
+            </Tooltip>
+          </div>
+        </div>
+        <div>
+          <Tooltip
+            title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+            placement="left-start"
+          >
+            <div>Total inkomst: {totalIncome}</div>
+          </Tooltip>
+        </div>
+      </div>
+      <br />
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
+        <div>
+          <div className="p-4">
+            <Tooltip
+              title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+              placement="left-start"
+            >
+              <TextField
+                label="kommunalskatt:"
+                defaultValue={municipalityTax}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </Tooltip>
+          </div>
+          <div className="p-4">
+            <Tooltip
+              title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+              placement="left-start"
+            >
+              <TextField
+                label="statskatt:"
+                defaultValue={stateTax}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </Tooltip>
+          </div>
+        </div>
+        <div>
+          <Tooltip
+            title="testasdfasdfasdfasdfasdf asd fasdf asdf"
+            placement="left-start"
+          >
+            <div>Total inkomst efter skatt: {incomeAfterTax}</div>
+          </Tooltip>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Calculator;
---
+++ src/app/components/LinkBox.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type LinkboxProps = {
+  name: string;
+  description: string;
+  href: string;
+  target: string;
+};
+const LinkBox = ({ name, description, href, target }: LinkboxProps) => (
+  <a
+    href={href}
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target={target}
+    rel="noopener noreferrer"
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      {name}{" "}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{description}</p>
+  </a>
+);
+
+export default LinkBox;
